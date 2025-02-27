884a949742f1b131d5b018fb420e485bca3f030e
Hemanth Kumar H N-Sahyadri-ECE <152687460+Hemanth4507@users.noreply.github.com>
2025-02-28 00:10:42 +0530

    Add files via upload

---
+++ TASK 4/hemanth_rv32i_tb.v
@@ -0,0 +1,23 @@
+module hemanth_rv32i_tb;
+
+reg clk,RN;
+wire [31:0]WB_OUT,NPC;
+
+hemanth_rv32i rv32(clk,RN,NPC,WB_OUT);
+
+
+always #3 clk=!clk;
+
+initial begin 
+RN  = 1'b1;
+clk = 1'b1;
+
+$dumpfile ("hemanth_rv32i.vcd"); //by default vcd
+$dumpvars (0, hemanth_rv32i_tb);
+  
+  #5 RN = 1'b0;
+  
+  #300 $finish;
+
+end
+endmodule
